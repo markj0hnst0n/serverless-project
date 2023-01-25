5f5821b03894968a9c6e660ad97796e53e4eb959
mark.j0hnst0n <friendorfaux6@gmail.com>
2023-01-25 14:46:18 +0000

    sqs consumer application added

---
+++ SqsConsumer/Program.cs
@@ -0,0 +1,39 @@
+﻿using Amazon.SQS;
+using SqsPublisher;
+using Amazon.SQS.Model;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+// Load Environment Variables from DotNetEnv Package
+
+DotNetEnv.Env.TraversePath().Load();
+
+var _accesskey = System.Environment.GetEnvironmentVariable("ACCESS_KEY");
+var _secretkey = System.Environment.GetEnvironmentVariable("SECRET_KEY");
+
+// Initialise new AWS SQS Client
+
+var cts = new CancellationTokenSource();
+var sqsClient = new AmazonSQSClient(_accesskey, _secretkey, Amazon.RegionEndpoint.EUWest1);
+
+var queueUrlResponse = await sqsClient.GetQueueUrlAsync("serverless-project-customers");
+
+var receiveMessageRequest = new ReceiveMessageRequest
+{
+	QueueUrl = queueUrlResponse.QueueUrl
+};
+
+while (!cts.IsCancellationRequested)
+{
+	var response = await sqsClient.ReceiveMessageAsync(receiveMessageRequest, cts.Token);
+
+	foreach (var message in response.Messages)
+	{
+		Console.WriteLine($"Message Id: {message.MessageId}");
+		Console.WriteLine($"Message Body: {message.Body}");
+
+		await sqsClient.DeleteMessageAsync(queueUrlResponse.QueueUrl, message.ReceiptHandle);
+	}
+
+	await Task.Delay(3000);
+}
